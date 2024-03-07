45e7c906989f2829ed0a16f030d75b992a445e27
Vinícius Maçulo <vinimaculo@gmail.com>
2024-03-06 22:06:40 -0300

    ajusta texto

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -22,8 +22,8 @@ export function About() {
                 <h1 className='font-bold text-4xl mb-5 tracking-wide lg:text-3xl lg:mb-3'>Vinícius Maçulo</h1>
                 <h2 className='font-bold text-4xl mb-5 lg:text-2xl lg:mb-3'>Sou <span className='text-orange-500'>Desenvolvedor Front-End</span></h2>
                 <p className='max-w-xl text-lg mb-8'>
-                Cursando o bacharelado em Sistemas de Informação no CEFET/RJ, dedico meu foco de estudos ao desenvolvimento web. 
-                Possuo habilidades sólidas em ReactJS, HTML, CSS e TypeScript, buscando constantemente aprimorar meu conhecimento nessas tecnologias e explorar novos desafios.
+                Poder criar interfaces atraentes e funcionais sempre me fascinou, e tenho dedicado tempo e esforço para aprofundar meus conhecimentos nessa área. 
+                Possuo habilidades sólidas em ReactJS, HTML, CSS e TypeScript, buscando constantemente aprimorar minha experiência nessas e em outras tecnologias.
                 </p>
 
                 <a href='#contact'><button className="border rounded-lg py-3 px-5 hover:scale-105 hover:bg-[#fed7aa21] duration-500 tracking-wider text-orange-500 border-orange-500">Contato</button></a>  
